92c829a98d8adb49ce42a6a0c32081122fa0438b
Rajan Joshi <razan.joc@gmail.com>
2019-03-03 23:52:39 +0000

    Isolate the logic to service object.

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,36 +1,10 @@
 class StaticController < ApplicationController
   def show
     if params[:input] && (1..3999).include?(params[:input].to_i)
-      @roman = find_roman(params[:input].to_i)
+      @roman = FindRoman.new(params[:input].to_i).call
     else
       @message = "Please enter between 1 to 3999"
     end
     render params[:page]
-  end
-
-  def find_roman(input)
-    roman_numbers = {
-        1000 => "M",  
-        900 => "CM",  
-        500 => "D",  
-        400 => "CD",
-        100 => "C",  
-          90 => "XC",  
-          50 => "L",  
-          40 => "XL",  
-          10 => "X",  
-            9 => "IX",  
-            5 => "V",  
-            4 => "IV",  
-            1 => "I",  
-      }
-    
-    n = input
-    roman = ""
-    roman_numbers.each do |value, letter|
-      roman << letter*(n / value)
-      n = n % value
-    end
-    return roman
   end
 end
---
+++ app/service_objects/find_roman.rb
@@ -0,0 +1,36 @@
+
+# Archive User from app
+class FindRoman
+  attr_reader :integer
+
+  ROMAN_NUMBERS = {
+    1000 => "M",  
+    900 => "CM",  
+    500 => "D",  
+    400 => "CD",
+    100 => "C",  
+      90 => "XC",  
+      50 => "L",  
+      40 => "XL",  
+      10 => "X",  
+        9 => "IX",  
+        5 => "V",  
+        4 => "IV",  
+        1 => "I",  
+  }
+
+  def initialize(integer)
+    @integer = integer
+  end
+
+  def call
+    n = integer
+    roman = ""
+    binding.pry
+    ROMAN_NUMBERS.each do |value, letter|
+      roman << letter*(n / value)
+      n = n % value
+    end
+    roman
+  end
+end
